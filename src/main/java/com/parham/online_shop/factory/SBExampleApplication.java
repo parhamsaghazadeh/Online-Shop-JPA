9da303c33913380461d04a85afdee63f240eeba1
parham <parhamsaghazadeh@gmail.com>
2025-12-05 18:21:02 +0330

    fix run spring

--- src/main/java/com/parham/online_shop/factory/SBExampleApplication.java
+++ src/main/java/com/parham/online_shop/factory/SBExampleApplication.java
@@ -3,10 +3,16 @@ package com.parham.online_shop.factory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
-@SpringBootApplication( scanBasePackages = {"com.parham.online_shop"})
+@SpringBootApplication(scanBasePackages = {"com.parham.online_shop"})
+@ComponentScan({"com.parham.online_shop.controller"})
+@EntityScan("com.parham.online_shop.entity")
+@EnableJpaRepositories("com.parham.online_shop.repository")
 public class SBExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SBExampleApplication.class, args);
