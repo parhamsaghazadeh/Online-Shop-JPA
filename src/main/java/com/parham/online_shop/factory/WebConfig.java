0137590579e67cad587f14fa9083d493265213fd
parham <parhamsaghazadeh@gmail.com>
2025-12-06 15:44:51 +0330

    fixed code

--- src/main/java/com/parham/online_shop/factory/WebConfig.java
+++ src/main/java/com/parham/online_shop/factory/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class WebConfig {
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer(){
-        return (factory)-> factory.setContextPath("/shop");
+        return (factory)-> factory.setContextPath("/online-shop");
     }
 }
