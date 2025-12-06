999f8be8da69e3c71242ee513f807285a1921134
parham <parhamsaghazadeh@gmail.com>
2025-12-06 20:29:00 +0330

    add field to Product

--- src/main/java/com/parham/online_shop/entity/Product.java
+++ src/main/java/com/parham/online_shop/entity/Product.java
@@ -4,6 +4,9 @@ package com.parham.online_shop.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Entity
 @Table(name = "product")
 @Data
@@ -13,6 +16,17 @@ public class Product {
     private Long id;
     @Column(name = "name", length = 50, nullable = false)
     private String name;
+    @Column (name = "brand" , length = 50, nullable = false)
+    private String brand;
+    @Column (name = "model" , length = 50, nullable = false)
+    private String model;
+    @Column(name = "made_in",length = 50, nullable = false)
+    private String madeIn;
+    @Column (name = "yare_of_menufacture",length = 50, nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date yareOfMenuFacture;
+    @Column (name = "price" , precision = 10, scale = 2, nullable = false )
+    private BigDecimal price;
     @ManyToOne
     @JoinColumn(name = "category_id", foreignKey = @ForeignKey(name = "FK_PRODUCT_CATEGORY_ID"))
     private Category category;
