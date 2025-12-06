d53c0c0b17671405a2c14bea2cdbe9f6b7a356b2
parham <parhamsaghazadeh@gmail.com>
2025-12-06 20:29:22 +0330

    add @PostMapping

--- src/main/java/com/parham/online_shop/controller/ProductController.java
+++ src/main/java/com/parham/online_shop/controller/ProductController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,17 @@ public class ProductController {
         } catch (Exception e) {
             log.error(e.getMessage());
             System.out.println(e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        try {
+            Product savedProduct = productService.addProduct(product);
+            return ResponseEntity.ok(savedProduct);
+        } catch (Exception e) {
+            log.error(e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
