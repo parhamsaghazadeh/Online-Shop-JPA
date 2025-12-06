235e07ab5ee4af68b43c646ddfafc5bee5ec9299
parham <parhamsaghazadeh@gmail.com>
2025-12-06 15:42:53 +0330

    add ProductController

---
+++ src/main/java/com/parham/online_shop/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.parham.online_shop.controller;
+
+import com.parham.online_shop.entity.Product;
+import com.parham.online_shop.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getProduct() {
+        try {
+            List<Product> products = productService.getAllProducts();
+            return ResponseEntity.ok(products);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
