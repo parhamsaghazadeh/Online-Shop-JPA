069c851c2aacbfcbaa5ee90228245aec88fc7ef7
parham <parhamsaghazadeh@gmail.com>
2025-12-07 21:50:10 +0330

    add CategoryRepository

---
+++ src/main/java/com/parham/online_shop/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.parham.online_shop.repository;
+
+import com.parham.online_shop.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
