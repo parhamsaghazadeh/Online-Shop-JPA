349a7b9448ca6770f8151ae952b8a686d8f2be41
parham <parhamsaghazadeh@gmail.com>
2025-12-08 22:17:21 +0330

    add ProductModel

---
+++ src/main/java/com/parham/online_shop/model/ProductModel.java
@@ -0,0 +1,76 @@
+package com.parham.online_shop.model;
+
+public class ProductModel {
+    private Long id;
+    private String name;
+    private String brand;
+    private String model;
+    private String madeIn;
+    private String year_of_manufacture;
+    private String price;
+    private Long category_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMadeIn() {
+        return madeIn;
+    }
+
+    public void setMadeIn(String madeIn) {
+        this.madeIn = madeIn;
+    }
+
+    public String getYear_of_manufacture() {
+        return year_of_manufacture;
+    }
+
+    public void setYear_of_manufacture(String year_of_manufacture) {
+        this.year_of_manufacture = year_of_manufacture;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
+}
