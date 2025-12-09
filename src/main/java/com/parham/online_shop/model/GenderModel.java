40c69771e5172a314a459033c103e3fb5663611d
parham <parhamsaghazadeh@gmail.com>
2025-12-09 19:47:38 +0330

    add GenderModel

---
+++ src/main/java/com/parham/online_shop/model/GenderModel.java
@@ -0,0 +1,23 @@
+package com.parham.online_shop.model;
+
+
+public class GenderModel {
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
