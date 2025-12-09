fd98c419f04c01391a8fd8d9fe00cfb4615b66b8
parham <parhamsaghazadeh@gmail.com>
2025-12-09 20:51:53 +0330

    add PersonService

---
+++ src/main/java/com/parham/online_shop/service/PersonService.java
@@ -0,0 +1,38 @@
+package com.parham.online_shop.service;
+
+import com.parham.online_shop.entity.Person;
+import com.parham.online_shop.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person addPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person updatePerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    public Person getPersonById(Long id) {
+        return personRepository.findById(id).orElse(null);
+    }
+}
