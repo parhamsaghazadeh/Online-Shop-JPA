2a74dbf47862a5e4f7c9baca2e2a0c4a6a2e23de
parham <parhamsaghazadeh@gmail.com>
2025-12-10 22:39:04 +0330

    add PersonService

--- src/main/java/com/parham/online_shop/model/Converter.java
+++ src/main/java/com/parham/online_shop/model/Converter.java
@@ -1,6 +1,7 @@
 package com.parham.online_shop.model;
 
 import com.parham.online_shop.entity.Category;
+import com.parham.online_shop.entity.Person;
 import com.parham.online_shop.entity.Product;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,8 @@ import java.util.stream.Collectors;
 @Component
 public class Converter {
     private static final DecimalFormat df = new DecimalFormat("#,##0.00");
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
 
     public ProductModel toModelProduct(Product product) {
 
@@ -44,5 +47,17 @@ public class Converter {
                 .stream().map(this::toModelProduct).collect(Collectors.toList());
         categoryModel.setProducts(productModels);
         return categoryModel;
+    }
+
+    public PersonModel toModelPerson(Person person) {
+        PersonModel personModel = new PersonModel();
+        personModel.setId(person.getId());
+        personModel.setName(person.getName());
+        personModel.setLastName(person.getLastName());
+        personModel.setBirthday(formatter.format(person.getBirthday()));
+        personModel.setUserId(person.getUserId());
+        personModel.setGenderId(person.getGenderId());
+        personModel.setRoleId(person.getRoleId());
+        return personModel;
     }
 }
