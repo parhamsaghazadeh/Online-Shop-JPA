2e459f795b33b15960fdeb251f26014738696216
parham <parhamsaghazadeh@gmail.com>
2025-12-12 15:16:24 +0330

    add RegisterModel

---
+++ src/main/java/com/parham/online_shop/model/RegisterModel.java
@@ -0,0 +1,12 @@
+package com.parham.online_shop.model;
+
+import lombok.Data;
+
+@Data
+public class RegisterModel {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
