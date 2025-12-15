d914596889af1fdadc464280d9463fef24f8f09f
parham <parhamsaghazadeh@gmail.com>
2025-12-15 22:05:34 +0330

    add OrderItemModel

---
+++ src/main/java/com/parham/online_shop/model/OrderItemModel.java
@@ -0,0 +1,49 @@
+package com.parham.online_shop.model;
+
+public class OrderItemModel {
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Long quantity;
+    private String price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
