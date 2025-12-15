663c14af77ea0551abc8d8fd3805753c000de76f
parham <parhamsaghazadeh@gmail.com>
2025-12-15 22:06:12 +0330

    add OrderModel

---
+++ src/main/java/com/parham/online_shop/model/OrderModel.java
@@ -0,0 +1,40 @@
+package com.parham.online_shop.model;
+
+public class OrderModel {
+    private long id;
+    private long personId;
+    private String paymentMethod;
+    private String paymentDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
