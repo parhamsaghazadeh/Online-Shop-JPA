170ec42cbcf266d62833cab46c7c9dc53a53e0c0
parham <parhamsaghazadeh@gmail.com>
2025-12-15 22:29:00 +0330

    fixe code

--- src/main/java/com/parham/online_shop/repository/OrderItemRepository.java
+++ src/main/java/com/parham/online_shop/repository/OrderItemRepository.java
@@ -1,4 +1,9 @@
 package com.parham.online_shop.repository;
 
-public interface OrderItemRepository {
+import com.parham.online_shop.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/com/parham/online_shop/repository/OrdersRepository.java
+++ src/main/java/com/parham/online_shop/repository/OrdersRepository.java
@@ -1,4 +1,9 @@
 package com.parham.online_shop.repository;
 
-public interface OrdersRepository {
+import com.parham.online_shop.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
 }
