4c47ee89f06d662e2f65d86eb6c4fb4569135e09
parham <parhamsaghazadeh@gmail.com>
2025-12-24 14:16:01 +0330

    add Location

--- src/main/java/com/parham/online_shop/entity/Location.java
+++ src/main/java/com/parham/online_shop/entity/Location.java
@@ -1,4 +1,21 @@
 package com.parham.online_shop.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "location")
+@Data
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title",length = 50, nullable = false)
+    private String title;
+    @Column(name = "type" , length = 50, nullable = false)
+    private String type;
+    @Column(name = "open_time" , length = 10, nullable = false)
+    private LocalDateTime openTime;
 }
