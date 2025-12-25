1b89257bf146ad07bf3a9730a18e2feff92afa04
parham <parhamsaghazadeh@gmail.com>
2025-12-25 12:57:44 +0330

    add DisplayService

--- src/main/java/com/parham/online_shop/service/DisplayService.java
+++ src/main/java/com/parham/online_shop/service/DisplayService.java
@@ -1,4 +1,24 @@
 package com.parham.online_shop.service;
 
+import com.parham.online_shop.entity.DisplayOrder;
+import com.parham.online_shop.model.DisplayOrderModel;
+import com.parham.online_shop.repository.DisplayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DisplayService {
+
+    private DisplayRepository displayRepository;
+
+    @Autowired
+    public DisplayService(DisplayRepository displayRepository) {
+        this.displayRepository = displayRepository;
+    }
+
+    public List<DisplayOrderModel> getAll() {
+        return displayRepository.findByPersonName();
+    }
 }
