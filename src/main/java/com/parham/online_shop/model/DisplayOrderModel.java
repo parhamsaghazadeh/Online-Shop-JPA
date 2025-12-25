3e9b4d85d4ec087b2ce2cd3b469d2aa9ae89fda7
parham <parhamsaghazadeh@gmail.com>
2025-12-25 23:21:29 +0330

    add DisplayOrderModel

--- src/main/java/com/parham/online_shop/model/DisplayOrderModel.java
+++ src/main/java/com/parham/online_shop/model/DisplayOrderModel.java
@@ -1,4 +1,164 @@
 package com.parham.online_shop.model;
 
 public class DisplayOrderModel {
+    //person
+    private String personName ;
+    private String lastName;
+    private String birthday;
+    //category
+    private String title;
+    //product
+    private String brand;
+    private String model;
+    private String yareOfMenuFacture;
+    private String name;
+    private String price;
+    //order
+    private String paymentDate;
+    private String paymentMethod;
+    //orderItem
+    private String quantity;
+    private String priceOrder;
+    //productRegistration
+    private String registrationDate;
+    //location
+    private String titleLocation;
+    private String type;
+    private String openTime;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYareOfMenuFacture() {
+        return yareOfMenuFacture;
+    }
+
+    public void setYareOfMenuFacture(String yareOfMenuFacture) {
+        this.yareOfMenuFacture = yareOfMenuFacture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPriceOrder() {
+        return priceOrder;
+    }
+
+    public void setPriceOrder(String priceOrder) {
+        this.priceOrder = priceOrder;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getTitleLocation() {
+        return titleLocation;
+    }
+
+    public void setTitleLocation(String titleLocation) {
+        this.titleLocation = titleLocation;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(String openTime) {
+        this.openTime = openTime;
+    }
 }
