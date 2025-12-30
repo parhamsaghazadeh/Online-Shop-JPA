4a97679d3184c74f288d35cf99718a971b4b6cea
parham <parhamsaghazadeh@gmail.com>
2025-12-30 22:51:34 +0330

    add new model

--- src/main/java/com/parham/online_shop/model/DisplayOrderModel.java
+++ src/main/java/com/parham/online_shop/model/DisplayOrderModel.java
@@ -1,177 +1,87 @@
 package com.parham.online_shop.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 public class DisplayOrderModel {
-    //person
-    private String personName ;
-    private String lastName;
-    private Date birthday;
-    //category
-    private String title;
-    //product
-    private String brand;
-    private String model;
-    private String madeIn;
-    private Date yareOfMenuFacture;
-    private String name;
-    private BigDecimal price;
-    //order
+    // person
+    private String personName;
+    private String personLastName;
+    private Date personBirthday;
+
+    // category
+    private String categoryTitle;
+
+    // product
+    private String productBrand;
+    private String productModel;
+    private String productMadeIn;
+    private Date productYear;
+    private String productName;
+    private BigDecimal productPrice;
+
+    // order
     private String paymentMethod;
     private LocalDateTime paymentDate;
-    //orderItem
-    private Long quantity;
-    private BigDecimal priceOrder;
-    //productRegistration
+
+    // order item
+    private long orderQuantity;
+    private BigDecimal orderItemPrice;
+
+    // registration
     private LocalDateTime registrationDate;
-    //location
-    private String titleLocation;
-    private String type;
-    private LocalDateTime openTime;
 
-    public String getPersonName() {
-        return personName;
-    }
+    // location
+    private String locationTitle;
+    private String locationType;
+    private LocalDateTime locationOpenTime;  // 👈 LocalTime
 
-    public void setPersonName(String personName) {
+    public DisplayOrderModel(
+            String personName,
+            String personLastName,
+            Date personBirthday,
+            String categoryTitle,
+            String productBrand,
+            String productModel,
+            String productMadeIn,
+            Date productYear,
+            String productName,
+            BigDecimal productPrice,
+            String paymentMethod,
+            LocalDateTime paymentDate,
+            long orderQuantity,
+            BigDecimal orderItemPrice,
+            LocalDateTime registrationDate,
+            String locationTitle,
+            String locationType,
+            LocalDateTime locationOpenTime
+    ) {
         this.personName = personName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getMadeIn() {
-        return madeIn;
-    }
-
-    public void setMadeIn(String madeIn) {
-        this.madeIn = madeIn;
-    }
-
-    public Date getYareOfMenuFacture() {
-        return yareOfMenuFacture;
-    }
-
-    public void setYareOfMenuFacture(Date yareOfMenuFacture) {
-        this.yareOfMenuFacture = yareOfMenuFacture;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
+        this.personLastName = personLastName;
+        this.personBirthday = personBirthday;
+        this.categoryTitle = categoryTitle;
+        this.productBrand = productBrand;
+        this.productModel = productModel;
+        this.productMadeIn = productMadeIn;
+        this.productYear = productYear;
+        this.productName = productName;
+        this.productPrice = productPrice;
         this.paymentMethod = paymentMethod;
-    }
-
-    public LocalDateTime getPaymentDate() {
-        return paymentDate;
-    }
-
-    public void setPaymentDate(LocalDateTime paymentDate) {
         this.paymentDate = paymentDate;
-    }
-
-    public Long getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Long quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getPriceOrder() {
-        return priceOrder;
-    }
-
-    public void setPriceOrder(BigDecimal priceOrder) {
-        this.priceOrder = priceOrder;
-    }
-
-    public LocalDateTime getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.orderQuantity = orderQuantity;
+        this.orderItemPrice = orderItemPrice;
         this.registrationDate = registrationDate;
+        this.locationTitle = locationTitle;
+        this.locationType = locationType;
+        this.locationOpenTime = locationOpenTime;
     }
 
-    public String getTitleLocation() {
-        return titleLocation;
-    }
-
-    public void setTitleLocation(String titleLocation) {
-        this.titleLocation = titleLocation;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public LocalDateTime getOpenTime() {
-        return openTime;
-    }
-
-    public void setOpenTime(LocalDateTime openTime) {
-        this.openTime = openTime;
-    }
 }
