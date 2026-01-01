08fcb5da359283cf5c779bbe3e9ea10bc111caa8
parham <parhamsaghazadeh@gmail.com>
2026-01-01 20:50:24 +0330

    join ProductRegisterModel

---
+++ src/main/java/com/parham/online_shop/model/ProductRegisterModel.java
@@ -0,0 +1,45 @@
+package com.parham.online_shop.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ProductRegisterModel {
+    private long id;
+    private long personId;
+    private long productId;
+    private LocalDateTime registrationDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
