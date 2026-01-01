11e25cda689659d6f4af2e8e60d1924ced6612ae
parham <parhamsaghazadeh@gmail.com>
2026-01-01 20:49:46 +0330

    join OrderByPerson

--- src/main/java/com/parham/online_shop/repository/PersonRepository.java
+++ src/main/java/com/parham/online_shop/repository/PersonRepository.java
@@ -1,7 +1,24 @@
 package com.parham.online_shop.repository;
 
 import com.parham.online_shop.entity.Person;
+import com.parham.online_shop.model.OrderByPersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Query("""
+            SELECT new com.parham.online_shop.model.OrderByPersonModel(
+            p.name , p.lastName , o.paymentMethod , o.paymentDate, ot.quantity,ot.price,pr.registrationDate
+            )
+                        from Orders o
+                        join o.person p
+                        join OrderItem ot on ot.orders = o
+                        join ProductRegistration pr on pr.person = p
+            
+            """)
+    List<OrderByPersonModel> findAllPerson();
 }
