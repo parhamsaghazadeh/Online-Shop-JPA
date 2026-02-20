b115a11d0c9e467b47d03ed8276e852229b2b380
Parham <parhamsaghazadeh@gmail.com>
2026-02-20 13:31:24 +0330

    add AddressModel

--- src/main/java/com/parham/online_shop/model/AddressModel.java
+++ src/main/java/com/parham/online_shop/model/AddressModel.java
@@ -1,4 +1,11 @@
 package com.parham.online_shop.model;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
 public class AddressModel {
+    private String city;
+    private String street;
+    private String zipcode;
 }
