7eeb87ea870a749bf6517a15110b3b46616c1a2f
Parham <parhamsaghazadeh@gmail.com>
2026-02-22 23:01:29 +0330

    add JpaSpecificationExecutor

--- src/main/java/com/parham/online_shop/repository/OrdersRepository.java
+++ src/main/java/com/parham/online_shop/repository/OrdersRepository.java
@@ -2,8 +2,9 @@ package com.parham.online_shop.repository;
 
 import com.parham.online_shop.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
+public interface OrdersRepository extends JpaRepository<Orders, Long>, JpaSpecificationExecutor<Orders> {
 }
