84e3613034220d9bd0bf555e7d01ec938d3eb712
Parham <parhamsaghazadeh@gmail.com>
2026-02-24 19:18:11 +0330

    add JpaSpecificationExecutor

--- src/main/java/com/parham/online_shop/repository/PersonRepository.java
+++ src/main/java/com/parham/online_shop/repository/PersonRepository.java
@@ -3,13 +3,14 @@ package com.parham.online_shop.repository;
 import com.parham.online_shop.entity.Person;
 import com.parham.online_shop.model.OrderByPersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person> {
     @Query("""
             SELECT new com.parham.online_shop.model.OrderByPersonModel(
             p.name , p.lastName , o.paymentMethod , o.paymentDate, ot.quantity,ot.price,pr.registrationDate
